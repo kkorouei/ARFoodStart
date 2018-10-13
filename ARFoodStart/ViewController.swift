34d3b2e41443383232b975380e4d466871e84138
Koushan Korouei <kkorouei@gmail.com>
2018-10-13 09:04:21 +0330

    Initial Commit

---
+++ ARFoodStart/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ARFoodStart
+//
+//  Created by Koushan Korouei on 13/10/2018.
+//  Copyright © 2018 Koushan Korouei. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create a new scene
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        
+        // Gestures
+        let tapGesure = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tapGesure)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    // MARK: - Gesture Recognizers
+    @objc func handleTap(gesture: UITapGestureRecognizer) {
+        let location = gesture.location(in: sceneView)
+        guard let hitTestResult = sceneView.hitTest(location, types: .existingPlane).first else { return }
+        let position = SCNVector3Make(hitTestResult.worldTransform.columns.3.x,
+                                      hitTestResult.worldTransform.columns.3.y,
+                                      hitTestResult.worldTransform.columns.3.z)
+    }
+}
